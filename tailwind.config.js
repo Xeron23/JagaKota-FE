77397b25ea96bcc48e53745941efcc8710e4eddf
Aria Lent <kaqyword2304@gmail.com>
2025-07-23 22:05:48 +0700

    init change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ export default {
   ],
   theme: {
     // change this config
-    colors: {
-      primary: '#1D4ED8',
-      secondary: '#64748B',
-    },
-    spacing: {
-      '128': '32rem',
-    },
-    fontFamily: {
-      sans: ['Inter', 'sans-serif'],
-    },
+    // colors: {
+    //   primary: '#1D4ED8',
+    //   secondary: '#64748B',
+    // },
+    // spacing: {
+    //   '128': '32rem',
+    // },
+    // fontFamily: {
+    //   sans: ['Inter', 'sans-serif'],
+    // },
     extend: {
       animation: {
           'spin-slow': 'spin 2s linear infinite',
