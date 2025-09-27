0b5ee39b84ab73cf14e5ba1b001d1690ffd0c2c0
xeron <kaqyword2304@gmail.com>
2025-09-28 01:26:14 +0700

    fix: not updated publicLayout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -14,7 +14,7 @@ export default function PublicLayout() {
           duration={700}
           onFinish={() => setIsLoading(false)}
           fullscreen
-        />
+        />    
       )}
 
       <div
