9761e2bf5bce7cd2c84250aca9fe78b77218dea3
xeron <kaqyword2304@gmail.com>
2025-09-28 01:21:56 +0700

    fix: not updated reportDetail

--- src/pages/ReportDetail/components/ReportInfo.jsx
+++ src/pages/ReportDetail/components/ReportInfo.jsx
@@ -53,7 +53,7 @@ const ReportInfo = ({ report }) => {
       month: "long",
       day: "numeric",
       hour: "2-digit",
-      minute: "2-digit",
+      minute: "2-digit",   
     });
 
   const username = localStorage.getItem("username");
